70fafdfa41a1071b52ec65e186ad9d9b3ed07b25
Douglas Cândido Amorim de Sena <d.amorim.de.sena@avanade.com>
2024-03-28 23:29:09 -0300

    adicionando modulos e rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TaskItemComponent } from './home/tasks-table/task-item.component';
+import { TaskPageComponent } from './task-page/task-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Rota para a página inicial
+  { path: 'new-task', component: TaskPageComponent }, // Rota para a página de criação de tarefas
+  { path: 'edit-task/:id', component: TaskPageComponent }, // Rota para a página de edição de tarefas
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,57 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
+import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
+import { ButtonModule } from 'primeng/button';
+import { DataViewModule } from 'primeng/dataview';
+import { TagModule } from 'primeng/tag';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
+import { HomeComponent } from './home/home.component';
+import { TooltipModule } from 'primeng/tooltip';
+import { TaskItemComponent } from './home/tasks-table/task-item.component';
+import { TaskTableComponent } from './home/tasks-table/tasks-table.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TaskPageComponent } from './task-page/task-page.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { DropdownModule } from 'primeng/dropdown';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { CardModule } from 'primeng/card';
+import { CalendarModule } from 'primeng/calendar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TaskItemComponent,
+    TopbarComponent,
+    TaskTableComponent,
+    TaskPageComponent,
   ],
   imports: [
+    BrowserAnimationsModule,
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    ButtonModule,
+    DataViewModule,
+    HttpClientModule,
+    TagModule,
+    TooltipModule,
+    ReactiveFormsModule,
+    FormsModule,
+    InputTextModule,
+    InputTextareaModule,
+    DropdownModule,
+    RadioButtonModule,
+    CardModule,
+    CalendarModule,
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideHttpClient(withFetch())
   ],
   bootstrap: [AppComponent]
 })
